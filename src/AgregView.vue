f7c33ac8932d738c3430e7e909eed79c7e68c586
Alice MORINIERE <alice.moriniere@gmail.com>
2025-09-11 16:45:12 +0200

    modif agreg

--- src/AgregView.vue
+++ src/AgregView.vue
@@ -1,1 +1,12 @@
-<template>TODO agreg</template>
+<template>
+<span> J'ai passé l'agrégation en 2024 en option A. Vous pouvez trouver ici l'entièreté de mes développements : mes documents sont souvent très longs, mais ils contiennent beaucoup de remarques/infos pas nécessaires pour le développement, mais juste pour essayer d'être paré.e face aux questions (en revanche ils sont tous rédigés à la main...). Il est fort probable que ces documens contiennent des coquilles, n'hésitez pas à me les signaler ! Voici un document qui résume mon couplage (leçon vers dev et dev vers leçon) : A FAIRE. Certaines associations sont probablement un peu tirées par les cheveux, il faut bien combler les trous... <span>
+<span>
+ 
+	<div class="flex flex-col m-5 justify-start">
+    <span class="text-2xl my-2 bold"> Développements :</span>
+    <span class="my-xp"
+      >Cours avec lien:
+      <a href="cours/main.pdf" target="_blank" class="underline">cliquer ici </a></span> 
+</div>
+
+</template>

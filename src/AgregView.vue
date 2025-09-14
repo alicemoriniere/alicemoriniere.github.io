607cded8db7409982eb0a57191e0ee404feed0c5
Alice MORINIERE <alice.moriniere@gmail.com>
2025-09-14 18:50:08 +0200

    ajout dev

--- src/AgregView.vue
+++ src/AgregView.vue
@@ -1,28 +1,81 @@
 <template>
-	<div class="flex m-10">
+	<div class="flex flex-col m-10" >
  <span> J'ai passé l'agrégation en 2024 en option A. Vous pouvez trouver ici l'entièreté de mes développements : mes documents sont souvent très longs, mais ils contiennent beaucoup de remarques/infos pas nécessaires pour le développement, mais juste pour essayer d'être paré.e face aux questions (en revanche ils sont tous rédigés à la main...). Il est fort probable que ces documens contiennent des coquilles, n'hésitez pas à me les signaler ! Voici un document qui résume mon couplage (leçon vers dev et dev vers leçon) : A FAIRE. Certaines associations sont probablement un peu tirées par les cheveux, il faut bien combler les trous... </span>
 
-	<div class="flex flex-col m-5 w-5/8 justify-start">
-      <span class="my-xp"
+	<div class="flex flex-col m-5 w-1/4 justify-center">
+      <span class="my-5"
       >  <a href="dev/automorph.pdf" target="_blank" class="underline">Automorphismes de Sn </ a></span>
-<span class="my-xp"
-      >  <a href="dev/automorph.pdf" target="_blank" class="underline">Automorphismes de Sn </ a></span> 
+<span class="my-5"
+      >  <a href="calculzeta/automorph.pdf" target="_blank" class="underline">Calcul des zeta 2k </ a></span> 
+<span class="my-5"
+      >  <a href="cardinalnilp/automorph.pdf" target="_blank" class="underline">Cardinal du cône nilpotent </ a></span> 
+<span class="my-5"
+      >  <a href="classifformesquad/automorph.pdf" target="_blank" class="underline">Classification des formes quadratiques sur Fq </ a></span> 
+<span class="my-5"
+      >  <a href="conditioncycl/automorph.pdf" target="_blank" class="underline">Condition de cyclicité des Z sur nZ croix </ a></span> 
+<span class="my-5"
+      >  <a href="cvfaiblehilbert/automorph.pdf" target="_blank" class="underline">Convergence faible et optimisation dans un Hilbert </ a></span> 
+<span class="my-5"
+      >  <a href="critkalmann/automorph.pdf" target="_blank" class="underline">Critère de Kalmann </ a></span>
+<span class="my-5"
+      >  <a href="decmatrice/automorph.pdf" target="_blank" class="underline">Décomposition de Dunford et exponentielle de matrices </ a></span>
+<span class="my-5"
+      >  <a href="decpolaire/automorph.pdf" target="_blank" class="underline">Décomposition polaire </ a></span>
+<span class="my-5"
+      >  <a href="decpolaire/automorph.pdf" target="_blank" class="underline">Dénombrement des polynômes irréductibles sur Fq </ a></span>
+</div> 
+
+
+
+<div class="flex flex-col m-5 w-1/4 ">
+      <span class="my-5"
+      >      <a href="dev/devasympto.pdf" target="_blank" class="underline">Développement asymptotique de suites définies par récurrence </ a></span> 
+<span class="my-5"
+      >      <a href="dev/diffdet.pdf" target="_blank" class="underline"> Différentielle du déterminant
+ </ a></span> 
+<span class="my-5"
+      >      <a href="dev/disqueger.pdf" target="_blank" class="underline"> Disques de Gershgorin
+ </ a></span> 
+<span class="my-5"
+      >      <a href="dev/envcvxon.pdf" target="_blank" class="underline"> Enveloppe convexe de On(R)
+ </ a></span> 
+<span class="my-5"
+      >      <a href="dev/eqbessel.pdf" target="_blank" class="underline"> Equation de Bessel
+ </ a></span> 
+<span class="my-5"
+      >      <a href="dev/eqchaleur.pdf" target="_blank" class="underline"> Equation de la chaleur
+ </ a></span> 
+<span class="my-5"
+      >      <a href="dev/formsmith.pdf" target="_blank" class="underline"> Forme normale de Smith
+ </ a></span> 
+<span class="my-5"
+      >      <a href="dev/formulestirling.pdf" target="_blank" class="underline"> Formule de Stirling par les intégrales de Wallis
+ </ a></span> 
+<span class="my-5"
+      >      <a href="dev/formuleeuler.pdf" target="_blank" class="underline"> Formule d'Euler Mac Laurin et appli à la série harmonique 
+ </ a></span> 
+<span class="my-5"
+      >      <a href="dev/genegln.pdf" target="_blank" class="underline"> Générateurs de GLn(K) et SLn(K) et connexité  </ a></span> 
+
 </div>
 
 
-<div class="flex flex-col m-5 w-5/8 ">
-      <span class="my-xp"
-      >      <a href="dev/automorph.pdf" target="_blank" class="underline">Automorphismes de Sn </ a></span> 
+
+<div class="flex flex-col m-5 w-1/4 ">
+<span class="my-5"
+      >      <a href="dev/gradpasopti.pdf" target="_blank" class="underline"> Gradient à pas optimal
+ </ a></span> 
+<span class="my-5"
+      >      <a href="dev/ineghoeffding.pdf" target="_blank" class="underline"> Inégalité de Hoeffding
+ </ a></span> 
+
+
+
 </div>
 
 
-<div class="flex flex-col m-5 w-5/8 ">
-      <span class="my-xp"
-      >      <a href="dev/automorph.pdf" target="_blank" class="underline">Automorphismes de Sn </ a></span> 
-</div>
 
-
-<div class="flex flex-col m-5 w-5/8 ">
+<div class="flex flex-col m-5 w-1/4 ">
       <span class="my-xp"
       >      <a href="dev/automorph.pdf" target="_blank" class="underline">Automorphismes de Sn </ a></span> 
 </div>

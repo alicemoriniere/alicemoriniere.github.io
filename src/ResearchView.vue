4c6988cf36d8e15233c279b80633e8cac82dc073
Alice MORINIERE <alice.moriniere@gmail.com>
2025-09-18 19:23:18 +0200

    ajout dans divers et recherche

--- src/ResearchView.vue
+++ src/ResearchView.vue
@@ -1,4 +1,25 @@
 <template>
-TO DO 
+<div class="flex flex-col m-10">
+    <span>
+      Je viens de commencer ma thèse sous la
+        supervision de
+        <a class="underline" href="https://jcbreton.pages.math.cnrs.fr/jcb/" _target="blank">
+          Jean-Christophe Breton,</a
+        >
+        sur le sujet "Théorèmes limite pour des modèles de boules aléatoires avec interaction". Je n'ai évidemment rien à ajouter ici pour l'instant, mais pour avoir une idée de mon sujet de thèse, vous pouvez jeter un oeil à mon <a href="rapportstage/stageM2.pdf" target="_blank" class="underline"> rapport de stage</a> de M2, notamment à la dernière partie qui présente un modèle de boules aléatoires dans le cadre poissonien. L'idée de la thèse serait de généraliser les théorèmes limite obtenus dans ce cas à des classes plus générales de processus ponctuels. 
+    </span>
+<span>
+      Pour rendre cette page moins vide et plus jolie, et pour illustrer mon propos, voilà quelques simulations de boules aléatoires dans le cas poissonien.    
+</span>
+
+<img src="./assets/joliesboules.png"  />
+  </div>
+	
 
  </template>
+
+<style scoped>
+.img-photo {
+  width: 19vw;
+}
+</style>

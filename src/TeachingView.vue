6f5e2a02988b28c7f915c97bd78a072cbebff351
Alice MORINIERE <alice.moriniere@gmail.com>
2026-04-09 17:31:33 +0200

    erreur ?

--- src/TeachingView.vue
+++ src/TeachingView.vue
@@ -4,7 +4,6 @@
       TP de la préparation à la modélisation en option A (agrégation) :</span
     >
     <span> J'ajouterai au fur et à mesure de l'année mes corrections et récaps de TP. Les fiches de TP (à l'exception de la 1bis) ont été créées par Emeline Luirard. </span>
-    <span> Voici le récap général pour la modé, qui regroupe tous les petits récaps de TP : <a href="tp/recapglobal.ipynb" target="_blank" class="underline">récap</a>. <span>
     <span class="text-2xl my-3 bold"> Enoncés et corrections des TP :</span>
     <!--grid-cols-n: nombre de colonnes, gap-x/y : espacement entre deux cellules -->
     <div class="grid grid-cols-9 gap-x-5 gap-y-5 mt-1">

fe5908940291d87ab9542ad2302f204fab26c5a1
Alice MORINIERE <alice.moriniere@gmail.com>
2026-05-05 12:51:46 +0200

    c'était moche

--- src/TeachingView.vue
+++ src/TeachingView.vue
@@ -26,6 +26,8 @@
 	<span> <a href="tp/correctiontp6.ipynb" target="_blank" download class="underline"> Correction </a>  </span>
 	<span>  <a href="tp/correctiontp7.ipynb" target="_blank" download class="underline"> Correction </a>  </span>
 	<span> <a href="tp/correctiontp8.py" target="_blank" download class="underline"> Correction </a> </span>
+	<span> </span>
+
 	<span> <a href="tp/recap1.ipynb" target="_blank" download  class="underline"> Recap </a> </span>
 	<span> <a href="tp/recap2.ipynb" target="_blank" download  class="underline"> Recap </a> </span>
 	<span> <a href="tp/recap1.ipynb" target="_blank" download  class="underline"> Recap </a> </span>
